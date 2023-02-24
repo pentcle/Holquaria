bf9b287154c68cfdb88c1362449f3a051c48446e
pentcle <pentacle@pentacle.xyz>
2023-02-24 23:46:26 +0000

    feat: get tw working and add some vars

--- src/pages/pokemon/index.js
+++ src/pages/pokemon/index.js
@@ -1,25 +1,25 @@
 import PokemonList from "@/components/PokemonList";
 
 
-export default function PokemonIndex({ pokemonArray }) {
-  console.log(pokemonArray);
-  return (
-    <div>
-    <h1 className="text-4xl">Pokemon List</h1>
-    <PokemonList pokemonArray={pokemonArray} />
-    </div>
-  );
+export default function PokemonIndex({pokemonArray}) {
+    console.log(pokemonArray);
+    return (
+        <div>
+            <h1 className="w-full mx-auto text-center text-4xl">Pokemon List</h1>
+            <PokemonList pokemonArray={pokemonArray}/>
+        </div>
+    );
 }
 
 export async function getStaticProps() {
-  const pokemonArray = [];
-  for (let i = 1; i < 21; i++) {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-    const data = await res.json();
-    pokemonArray.push(data);
-  }
+    const pokemonArray = [];
+    for (let i = 1; i < 21; i++) {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        const data = await res.json();
+        pokemonArray.push(data);
+    }
 
-  return {
-    props: { pokemonArray },
-  };
+    return {
+        props: {pokemonArray},
+    };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,58 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./src/**/*.js',],
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "bg-01": "var(--bg-01)",
+      },
+      fontSize: {
+        xs: '0.688rem',
+        sm: '0.813rem',
+        base: '0.875rem',
+        lg: '1rem',
+        xl: '1.125rem',
+        '2xl': '1.5rem',
+        '3xl': '1.75rem',
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+        '6xl': '3rem',
+      },
+      spacing: {
+        '128': '32rem',
+        '160': '40rem',
+      },
+      fontFamily: {
+        'font-mono': "var(--font-mono)",
+      },
+      colors: {
+        "callout-rgb": "var(--callout-rgb)",
+        "callout-border-rgb": "var(--callout-border-rgb)",
+        "card-rgb": "var(--card-rgb)",
+        "card-border-rgb": "var(--card-border-rgb)",
+        "secondary-glow": "var(--secondary-glow)",
+        "primary-glow": "var(--primary-glow)",
+        "tile-border": "var(--tile-border)",
+        "tile-start-rgb": "var(--tile-start-rgb)",
+        "tile-end-rgb": "var(--tile-end-rgb)",
+        "background-end-rgb": "var(--background-end-rgb)",
+        "background-start-rgb": "var(--background-start-rgb)",
+      },
+      borderRadius: {
+        'border-radius': "var(--border-radius)",
+      },
+      zIndex: {
+        '60': '60',
+        '100': '100',
+        '140': '140',
+        '160': '160',
+        '200': '200',
+        '240': '240',
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("autoprefixer")],
 }
